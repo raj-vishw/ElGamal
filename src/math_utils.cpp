fcc9c02f8df53418b1bdb6b497f0e0280fb8ad5f
raj-vishw <vishwkarmaraja2222@gmail.com>
2026-04-21 10:07:13 -0400

    added modExp and modInverse

---
+++ src/math_utils.cpp
@@ -0,0 +1,38 @@
+#include "../include/math_utils.h"
+
+// Fast Module exponentiation
+cpp_int modExp(cpp_int base, cpp_int exp, cpp_int mod){
+    cpp_int result = 1;
+    base %= mod;
+
+    while(exp > 0){
+        if(exp % 2 == 1) result = (result * base)%mod;
+        
+        base = (base * base) %mod;
+        exp /= 2;
+    }
+    return result;
+}
+
+//EEA
+cpp_int modInverse(cpp_int a, cpp_int mod){
+    cpp_int m0 = mod, t, q;
+    cpp_int x0 = 0, x1 = 1;
+
+    if(mod==1) return 0;
+    while(a>1){
+        q = a/mod;
+        t = mod;
+
+        mod = a % mod;
+        a = t;
+        t = x0;
+
+        x0 = x1-q*x0;
+        x1 = tl
+    }
+    if(x1 < 0){
+        x1 += m0;
+    }
+    return x1;
+}
