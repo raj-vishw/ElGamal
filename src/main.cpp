8f9bb3c8297a33299ac6cde5c63245e255cf33c3
raj-vishw <vishwkarmaraja2222@gmail.com>
2026-04-22 00:11:43 -0400

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,13 @@ int main() {
     PrivateKey priv;
 
     generateKeys(pub, priv);
-
-    cpp_int message = 12345;
-
+    std::cout<<"Enter a message(number):";
+    cpp_int message;
+    std::cin>>message;
     std::cout << "Original: " << message << std::endl;
-
     CipherText cipher = encrypt(message, pub);
 
     std::cout << "Encrypted: (" << cipher.c1 << ", " << cipher.c2 << ")\n";
-
     cpp_int decrypted = decrypt(cipher, pub, priv);
 
     std::cout << "Decrypted: " << decrypted << std::endl;
